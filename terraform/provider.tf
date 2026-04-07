c15b10c8c88280a2714b7ee7e1beef9fa682cf6e
Pedro Melendez <pemelend@google.com>
2026-04-07 10:04:55 -0700

    Removed GCS to keep Terraform state

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,13 +5,10 @@ terraform {
       version = "~> 7.0" 
     }
   }
-  backend "gcs" {
-    bucket = "opm-tests-tfstate" # <--- Change this to your bucket!
-    prefix = "terraform/state"
-  }
 }
 
 provider "google" {
   project = var.project_id
   region  = var.region
 }
+
